2f99807dde6b2c937ce5080844502b83b6d3b24a
leonlin14 <leonlin14@gmail.com>
2014-11-24 08:24:19 +0800

    remove page

--- routes/hello.js
+++ routes/hello.js
@@ -7,10 +7,6 @@ exports.index = function(req, res){
   res.render('hello');
 };
 
-exports.jollen = function(req, res){
-  res.render('jollen');
-};
-
 exports.hi = function(req, res){
   res.render('hi');
 };
