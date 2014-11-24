a4c8b7f6af7ea7c0d5b3bc0ec6fbe4fe5bd451e1
leonlin14 <leonlin14@gmail.com>
2014-11-24 08:26:00 +0800

    implement Improve UX, #1

--- public/javascripts/index-age.js
+++ public/javascripts/index-age.js
@@ -8,9 +8,8 @@ var app = app || {};
 **/
 app.Users = Backbone.Model.extend({
   url: function() {
-    return '/1/user/age' + this.filter;
+    return '/1/user';
   },
-  filter: '/30/40/',
   defaults: {
     errors: [],
     errfor: {},
@@ -28,20 +27,46 @@ app.ListView = Backbone.View.extend({
     'click #filter': 'onFilter'
   },
   initialize: function() {
+    var self = this;
+
     this.model = new app.Users();
     this.listenTo(this.model, 'sync', this.render);
     this.listenTo(this.model, 'change', this.render);
-    this.model.fetch();
+    this.model.fetch({
+      success: function() {
+        var users = self.model.get('users');
+
+        users.sort(function(a, b) {
+          if(a.Age > b.Age) return 1;
+          else if(a.Age < b.Age) return -1;
+          return 0;
+        });
+      }
+    });
   },
   render: function() {
     this.$el.html(this.template( this.model.attributes ));
+
+    this.$el.find('[data-tag=user]').each(function () {
+       var me = $(this);
+       var age = '' + me.data('age');
+
+       console.log(age);
+       me.addClass('age-' + age.slice(0, 1) + '0');
+    });
   },
   onFilter: function(e) {
     var me = $(e.target);
     var filter = me.data('filter');
 
-    this.model.filter = filter;
-    this.model.fetch();
+    this.$el.find('[data-tag=user]').each(function () {
+      $(this).addClass('hide');
+    });
+
+    console.log(filter);
+    this.$el.find('.' + filter).each(function () {
+      $(this).removeClass('hide');
+    });
   },
 });
 
